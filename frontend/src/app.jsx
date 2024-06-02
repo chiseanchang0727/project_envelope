b7704ee753e9d0d5e9a2fbfe8f8fd12c9d6f617a
chiseanchang0727 <luciuskyear0812@gmail.com>
2024-06-02 21:13:48 +0800

    frontend update

---
+++ frontend/src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./MainPage"
+
+
+const root = createRoot(document.getElementsById("app"));
+
+root.render(<App />);
